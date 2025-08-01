85428c74076ed8da703d22638b57c0a58e9241f1
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-08-01 11:47:10 +0200

    feat(PriceTable): edit style import

--- src/components/organisms/PriceTable/PriceTable.tsx
+++ src/components/organisms/PriceTable/PriceTable.tsx
@@ -1,7 +1,7 @@
 
 import { CheckLg } from 'react-bootstrap-icons'
 import { Button, Card } from '../../atoms'
-import styles from './PriceTable.module.css'
+import './PriceTable.modules.css'
 
 type Service = {
     serviceType: string,
@@ -19,18 +19,18 @@ export function PriceTable({ services }: ServicesProp) {
     return (
         <>
             <section>
-                <div className={styles.head}>
+                <div className={'head'}>
                     <h1>Transparente Preise für alle</h1>
                     <span>bei uns finden sie akutelle faire Preise. Bei hjedem Service sind Preis und die behinhaltet Service aufgelistet.</span>
                 </div>
                 <div className='d-flex flex-wrap gap-4 justify-content-center'>
                     {
                         services.map((service, index) => (
-                            <Card key={index} className={styles.card}>
+                            <Card key={index} className={'card'}>
                                 <span><strong>{service.serviceType}</strong></span>
                                 <span>{service.price}€</span>
                                 <span>{service.description}</span>
-                                <div className={styles.features}>
+                                <div className={'features'}>
                                     {
                                         service.features.map((feature, i) => (
                                             <span key={i}><CheckLg color='green' /> {feature}</span>
